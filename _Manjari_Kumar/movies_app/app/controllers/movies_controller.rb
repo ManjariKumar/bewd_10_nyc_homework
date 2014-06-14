194d37b24823554cf4019510907140c8856a9fc7
ManjariKumar <manjari.kumar.ny@gmail.com>
2014-06-14 11:24:58 -0400

    Movies App Add new movie and Show specific movie by id working

--- _Manjari_Kumar/movies_app/app/controllers/movies_controller.rb
+++ _Manjari_Kumar/movies_app/app/controllers/movies_controller.rb
@@ -5,10 +5,18 @@ class MoviesController < ApplicationController
 	end
 
 	def show
-		# @movie = Movie.find(params[:id])
+		@movie = Movie.find(params[:id])
 	end
 
 	def create 
+		@movie = Movie.new(movie_params)
+		@movie.save
+		redirect_to @movie
+	end
+
+	private 
+	def movie_params
+    params.require(:movie).permit(:title, :description, :year_released)
 	end
 
 end
